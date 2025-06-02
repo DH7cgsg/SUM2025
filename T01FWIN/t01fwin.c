49fe477f736ed6c7bc2ddba6faa0025f496f8a29
Хиелевский Даниил (DH7) <cgsg113@ad.pml30.ru>
2025-06-02 18:02:45 +0300

    Added FWIN project.

---
+++ T01FWIN/t01fwin.c
@@ -0,0 +1,117 @@
+/* FILE NAME  : t01fwin.c
+ * PROGRAMMER : DH7
+
+ * LAST UPDATE: 02.06.2025
+ * PURPOSE    : First windowed application project.
+ *              Startup entry-point module.
+ */
+
+#include <windows.h>
+#define WND_CLASS_NAME "my window"
+LRESULT CALLBACK MainWindowFunc( HWND hWnd, UINT Msg,
+                              WPARAM wParam, LPARAM lParam );
+#define _WIN32_WININT 0x0500
+ 
+/* Main startup program function.
+ * ARGUMENTS:
+ *   - application instance handle:
+ *       HINSTANCE hInstance;
+ *   - previouse application instance handle
+ *     (not used, alway NULL):
+ *       HINSTANCE hPrevInstance;
+ *   - command line string:
+ *       CHAR *CmdLine;
+ *   - command line window show parameter (see SW_***):
+ *       INT CmdShow;
+ * RETURNS:
+ *   (INT) system error level value.
+ */
+INT WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                    CHAR *CmdLine, INT CmdShow )
+{
+  WNDCLASS wc;
+  HWND hWnd;
+  MSG msg;
+
+  wc.style = CS_HREDRAW | CS_VREDRAW;
+  wc.cbWndExtra = 0;
+  wc.cbClsExtra = 0;
+  wc.hbrBackground = (HBRUSH)COLOR_MENUTEXT;
+  wc.hCursor = LoadCursor(NULL, IDC_CROSS);
+  wc.hIcon = LoadIcon(NULL, IDI_SHIELD);
+  wc.lpszMenuName = NULL;
+  wc.hInstance = hInstance;
+  wc.lpfnWndProc = MainWindowFunc;
+  wc.lpszClassName = WND_CLASS_NAME;
+
+  if (!RegisterClass(&wc))
+  {
+    MessageBox(NULL, "error trying to register window class", "ERROR", MB_OK);
+    return 0;
+  }
+  hWnd = CreateWindow(WND_CLASS_NAME,
+                      "first window",
+                      WS_OVERLAPPEDWINDOW,
+                      CW_USEDEFAULT, CW_USEDEFAULT,
+                      CW_USEDEFAULT, CW_USEDEFAULT,
+                      NULL,
+                      NULL,
+                      hInstance,
+                      NULL);
+  ShowWindow(hWnd, SW_SHOWNORMAL);
+  
+
+  while (GetMessage(&msg, NULL, 0, 0))
+  {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+   
+    
+  }
+  return msg.wParam;
+
+
+} 
+/* End of 'WinMain' function */
+ 
+ 
+/* Window handle function.
+ * ARGUMENTS:
+ *   - window handle:
+ *       HWND hWnd;
+ *   - message type (see WM_***):
+ *       UINT Msg;
+ *   - message 'word' parameter:
+ *       WPARAM wParam;
+ *   - message 'long' parameter:
+ *       LPARAM lParam;
+ * RETURNS:
+ *   (LRESULT) message depende return value.
+ */
+LRESULT CALLBACK MainWindowFunc( HWND hWnd, UINT Msg,
+                              WPARAM wParam, LPARAM lParam )
+{
+  HDC hDC;
+  PAINTSTRUCT ps;
+  HBRUSH hBr;
+
+  //hBr = CreateSolidBrush(RGB(0, 0, 255));
+  switch (Msg)
+  {
+  case WM_DESTROY:
+    PostQuitMessage(0);
+    return 0;
+  case WM_PAINT:
+    hDC = BeginPaint(hWnd, &ps);
+    Ellipse(hDC, 500, 200, 900, 500);
+    SelectObject(hDC, GetStockObject(DC_BRUSH));
+    SetDCBrushColor(hDC, RGB(0, 0, 255));
+    Ellipse(hDC, 620, 270, 770, 420);
+    EndPaint(hWnd, &ps);
+    return 0;
+  }
+  return DefWindowProc(hWnd, Msg, wParam, lParam);
+}
+/* End of 'MainWindowFunc' function */
+ 
+/* END OF 't01fwin.c' FILE */
